8d301ae5f181d7f001d7dd023ba81fe3ffd68c6c
vcokltfre <vcokltfre@gmail.com>
2021-05-26 23:50:23 +0100

    feat: add pixels methods to db

--- pixels/utils/database.py
+++ pixels/utils/database.py
@@ -2,6 +2,7 @@ from os import getenv
 
 from aioredis import ConnectionsPool, create_pool as create_redis_pool
 from asyncpg import Pool, create_pool
+from more_itertools import chunked
 
 
 class Database:
@@ -19,3 +20,21 @@ class Database:
 
         with open("./pixels/migrations/0001-init.sql") as f:
             await self.pool.execute(f.read())
+
+        await self.setup_redis()
+
+    async def setup_redis(self) -> None:
+        if not await self.redis.execute("GET", "pixels_state"):
+            state = await self.pool.fetchrow("SELECT * FROM PixelState ORDER BY id DESC LIMIT 1;")
+            if state:
+                pixels = bytes.fromhex(state["pixels"])
+            else:
+                pixels = bytes.fromhex("000000"*(128**2))
+            await self.redis.execute("SET", "pixels_state", pixels)
+
+    async def get_pixels(self) -> bytearray:
+        return await self.redis.get("pixels_state")
+
+    async def set_pixel(self, x: int, y: int, value: str) -> None:
+        offset = (y * 128 + x) * 3
+        await self.redis.execute("SETRANGE", offset, bytes.fromhex(value))
